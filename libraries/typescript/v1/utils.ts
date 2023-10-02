f5325b8c059480f0f84547684833e1ffe7947bfd
Andrés Angulo <andres@angulo.org>
2023-10-02 14:31:16 +0200

    Bugfix of util functions

--- libraries/typescript/v1/utils.ts
+++ libraries/typescript/v1/utils.ts
@@ -1,6 +1,6 @@
-import { CriteriaTreeElement, Criterion, Task } from './types';
+import { CriteriaTreeElement, Criterion, Task, TaskGroup, TaskItem } from './types';
 
 export const isCriterion = (element: CriteriaTreeElement): element is Criterion => element.type === 'criterion';
-export const isTaskGroup = (element: CriteriaTreeElement): element is Criterion => element.type === 'task-group';
-export const isTask = (element: CriteriaTreeElement): element is Criterion => element.type === 'task';
-export const isTaskItem = (element: CriteriaTreeElement): element is Criterion => element.type === 'task-item';
+export const isTaskGroup = (element: CriteriaTreeElement): element is TaskGroup => element.type === 'task-group';
+export const isTask = (element: CriteriaTreeElement): element is Task => element.type === 'task';
+export const isTaskItem = (element: CriteriaTreeElement): element is TaskItem => element.type === 'task-item';
