c37d7aae70d75efdefc7f1fb5fa5c08507a6e315
Andrés Angulo <andres@angulo.org>
2023-10-19 19:13:50 +0200

    Adds correct package configuration

--- libraries/python/setup.py
+++ libraries/python/setup.py
@@ -2,12 +2,12 @@ from setuptools import find_packages, setup
 
 setup(
     name='openframe_criteria_set_protocol',
-    packages=find_packages(include=['openframe_criteria_set_protocol']),
-    version='0.0.0',
+    packages=find_packages(),
+    version='0.0.7',
     description='A protocol and tools for defining and working with criteria sets',
     author='Andrés Angulo <aa@openframe.org>',
     install_requires=[],
     setup_requires=['pytest-runner'],
     tests_require=['pytest==4.4.1'],
-    test_suite='tests',
+    test_suite='tests'
 )
