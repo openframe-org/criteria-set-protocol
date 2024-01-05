5c5bd341ba1ec7bd5cb0cdc6227c5bc74a6d96ed
Andrés Angulo <andres@angulo.org>
2024-01-05 13:55:20 +0100

    Makes parameters and values not required

--- libraries/python/openframe_criteria_set_protocol/v1/schemas.py
+++ libraries/python/openframe_criteria_set_protocol/v1/schemas.py
@@ -6,6 +6,6 @@ version = r'^(0|[1-9][0-9]*)\.(0|[1-9][0-9]*)\.(0|[1-9][0-9]*)$'
 
 class TreeAndMatrixBodySchema(Schema):
     locale = fields.Str(required=False)
-    values = fields.Dict(required=True)
-    parameters = fields.Dict(required=True)
+    values = fields.Dict(required=False)
+    parameters = fields.Dict(required=False)
 
