f9f143c19cd7d8350432143a6bc448ff5d25bb8f
Andrés Angulo <andres@angulo.org>
2024-05-10 12:10:24 +0200

    Adds find in tree util

--- libraries/python/openframe_criteria_set_protocol/v1/utils.py
+++ libraries/python/openframe_criteria_set_protocol/v1/utils.py
@@ -1,15 +1,21 @@
 from typing import Optional
 
-from .types import Quality, CriteriaTreeElement, Criterion, TaskGroup, Task
+from .types import Quality, CriteriaTreeElement, Criterion, TaskGroup, Task, CriteriaTree
 
 
 def to_color_hex_string(color):
+    """
+    Convert a color object to a hex string
+    """
     if isinstance(color, str):
         return color
     return f"#{color.red:02x}{color.green:02x}{color.blue:02x}"
 
 
 def should_hide_code(element: CriteriaTreeElement | str | dict) -> bool:
+    """
+    Check if a tree element should be hidden in the output
+    """
     if isinstance(element, str):
         return element.startswith('_')
     if isinstance(element, dict):
@@ -21,6 +27,9 @@ def should_hide_code(element: CriteriaTreeElement | str | dict) -> bool:
 
 
 def get_qualified_name(element: Quality | Criterion | TaskGroup | Task | dict) -> str:
+    """
+    Get the qualified name of a tree element, which is the title with the code prepended if it is different
+    """
     if isinstance(element, dict):
         title, code = (element.get('title', None), element.get('code', None))
         if title is None or code is None:
@@ -35,6 +44,9 @@ def get_qualified_name(element: Quality | Criterion | TaskGroup | Task | dict) -
 
 
 def resolve_code(element: CriteriaTreeElement | str | dict) -> str:
+    """
+    Get the code for a tree element, stripping away unnecessary characters
+    """
     if isinstance(element, str):
         resolved_code = element
     elif isinstance(element, dict):
@@ -44,3 +56,25 @@ def resolve_code(element: CriteriaTreeElement | str | dict) -> str:
     else:
         resolved_code = element.code
     return resolved_code[1:] if resolved_code.startswith('_') else resolved_code
+
+
+def find_in_tree(tree: CriteriaTree, code: str) -> Optional[CriteriaTreeElement]:
+    """
+    Find an element in the criteria tree by its code
+    """
+    for quality in tree.qualities:
+        if quality.code == code:
+            return quality
+        for criteria in quality.items:
+            if criteria.code == code:
+                return criteria
+            for task_group in criteria.items:
+                if task_group.code == code:
+                    return task_group
+                for task in task_group.items:
+                    if task.code == code:
+                        return task
+                    for task_item in task.items:
+                        if task_item.code == code:
+                            return task_item
+    return None
--- libraries/typescript/v1/utils.ts
+++ libraries/typescript/v1/utils.ts
@@ -1,4 +1,4 @@
-import { Color, CriteriaTreeElement, Quality, Criterion, Task, TaskGroup, TaskItem } from './types';
+import { Color, CriteriaTree, CriteriaTreeElement, Quality, Criterion, Task, TaskGroup, TaskItem } from './types';
 
 export const isQuality = (element: CriteriaTreeElement): element is Quality => element.type === 'quality';
 export const isCriterion = (element: CriteriaTreeElement): element is Criterion => element.type === 'criterion';
@@ -6,6 +6,9 @@ export const isTaskGroup = (element: CriteriaTreeElement): element is TaskGroup 
 export const isTask = (element: CriteriaTreeElement): element is Task => element.type === 'task';
 export const isTaskItem = (element: CriteriaTreeElement): element is TaskItem => element.type === 'task-item';
 
+/**
+ * Convert a color object to a hex string
+ */
 export const toColorHexString = (color: Color) => {
   if (typeof color === 'string') {
     return color;
@@ -14,10 +17,16 @@ export const toColorHexString = (color: Color) => {
   return `#${color.red.toString(16)}${color.green.toString(16)}${color.blue.toString(16)}`;
 };
 
+/**
+ * Check if a tree element should be hidden in the output
+ */
 export const shouldHideCode = (element: CriteriaTreeElement | { code: string } | string): boolean => {
   return (typeof element === 'string' ? element : element.code).startsWith('_');
 };
 
+/**
+ * Get the qualified name of a tree element, which is the title with the code prepended if it is different
+ */
 export const getQualifiedName = (element: Quality | Criterion | TaskGroup | Task | { code: string, title: string }): string => {
   const code = element.code.startsWith('_') ? element.code.substring(1) : element.code;
   if (element.title === code) {
@@ -27,7 +36,48 @@ export const getQualifiedName = (element: Quality | Criterion | TaskGroup | Task
   return `${code} ${element.title}`;
 };
 
+/**
+ * Get the code for a tree element, stripping away unnecessary characters
+ */
 export const resolveCode = (element: CriteriaTreeElement | { code: string } | string): string => {
   const resolvedCode = typeof element === 'string' ? element : element.code;
   return resolvedCode.startsWith('_') ? resolvedCode.substring(1) : resolvedCode;
 };
+
+
+/**
+ * Find an element in the criteria tree by its code
+ */
+export const findInTree = (tree: CriteriaTree, code: string): CriteriaTreeElement | null => {
+  for (const quality of tree.qualities) {
+    if (quality.code === code) {
+      return quality;
+    }
+
+    for (const criteria of quality.items) {
+      if (criteria.code === code) {
+        return criteria;
+      }
+
+      for (const taskGroup of criteria.items) {
+        if (taskGroup.code === code) {
+          return taskGroup;
+        }
+
+        for (const task of taskGroup.items) {
+          if (task.code === code) {
+            return task;
+          }
+
+          for (const taskItem of task.items) {
+            if (taskItem.code === code) {
+              return taskItem;
+            }
+          }
+        }
+      }
+    }
+  }
+
+  return null;
+}
